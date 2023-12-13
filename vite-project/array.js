d20d4c04fea9669ead627df8e99b07b2b560c794
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2023-12-13 13:12:40 -0500

    47kw

--- vite-project/array.js
+++
@@ -1,9 +0,0 @@
-const URL= "https://any-api.com/nba_com/nba_com/docs/_allstarballotpredictor"
-
-async function getData(URL){
-    try {
-        const response = await fetch(URL)
-        const data = await response.json()
-        console.log(data)
-    }
-}
