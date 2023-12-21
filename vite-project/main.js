03cb1c2e3713c5bb96e47f7d29d2575ab289d0c4
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2023-12-21 12:23:41 -0500

    teuw

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,16 +1,6 @@
-import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from './vite.svg';
-import { setupCounter } from './counter.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
     <h1>Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
@@ -21,25 +11,16 @@ document.querySelector('#app').innerHTML = `
   </div>
 `;
 
-TODAY = datetime.today()
-BASE_URL = 'http://stats.nba.com/stats/{endpoint}'
-HEADERS = {
-    'user-agent': ('Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'),  # noqa: E501
-    'Dnt': ('1'),
-    'Accept-Encoding': ('gzip, deflate, sdch'),
-    'Accept-Language': ('en'),
-    'origin': ('http://stats.nba.com')
-    }
+
+async function check(){
+  
+}
 
 
 
 
 
 
-
-
-
-setupCounter(document.querySelector('#counter'));
 
 function greet(name) {
   return new Promise(function(resolve, reject) {
@@ -52,7 +33,7 @@ nuggies.then((result) => {
   console.log(result);
 });
 
-const URL = ''; // Replace with your URL
+const URL = 'https://www.balldontlie.io/api/v1/players'; // Replace with your URL
 async function getData(URL) {
   try {
     const response = await fetch(URL);
@@ -60,7 +41,8 @@ async function getData(URL) {
     if (response.status !== 200) {
       throw new Error(response.statusText);
     }
-    const data = await response.json();
+    const data = await response.data.json();
+    console.log(data)
     document.querySelector("h1").textContent = data.content;
     document.querySelector("h2").textContent = data.author;
   } catch (error) {
@@ -70,11 +52,10 @@ async function getData(URL) {
 
 getData(URL);
 
-const apiURL = "http://stats.nba.com";
 
-async function fetchAPIData(apiURL) {
+async function fetchAPIData(URL) {
   try {
-    const response = await fetch(apiURL);
+    const response = await fetch(URL);
     const data = await response.json();
     console.log(data);
   } catch (error) {
@@ -82,7 +63,7 @@ async function fetchAPIData(apiURL) {
   }
 }
 
-fetchAPIData(apiURL);
+fetchAPIData(URL);
 
 const DOMSelectors = {
   button: document.querySelector('#yourButtonSelector'), // Replace with your valid selector
