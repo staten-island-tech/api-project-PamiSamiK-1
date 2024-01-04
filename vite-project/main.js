7761403e21426591da468bf83f9c6ad7a42d77aa
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2024-01-03 22:04:14 -0500

    3q2gyrw

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,6 +1,5 @@
 const API_URL = 'https://www.balldontlie.io/api/v1/players';
 
-// Fetch data from the API
 async function fetchDataFromAPI(URL) {
   try {
     const response = await fetch(URL);
@@ -15,8 +14,7 @@ async function fetchDataFromAPI(URL) {
     document.querySelector("h1").textContent = data.player;
     document.querySelector("h2").textContent = data.height;
 
-    
-    injectCards(data); 
+    injectCards(data.data); 
   } catch (error) {
     console.error("Error fetching API data:", error);
     document.querySelector("h1").textContent = error.message;
@@ -30,10 +28,10 @@ const DOMSelectors = {
 
 function injectCards(peopleList) {
   DOMSelectors.gallery.innerHTML = '';
-  peopleList.forEach(person => {
+  peopleList.forEach(player => {
     DOMSelectors.gallery.insertAdjacentHTML("beforeend", `
       <div class="card">
-        <h2>${person.first_name} ${person.last_name}</h2>
+        <h2>${player.first_name} ${player.last_name}</h2>
         <!-- Add other details -->
       </div>
     `);
@@ -41,4 +39,3 @@ function injectCards(peopleList) {
 }
 
 fetchDataFromAPI(API_URL);
-
